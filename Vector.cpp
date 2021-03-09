c20af331a0906dfb56326205437c70a8dc28efdc
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2021-03-09 12:44:36 +0100

    Fixed reading of nonexisiting PGM file to vector.

--- Vector.cpp
+++ Vector.cpp
@@ -135,7 +135,12 @@ bool Vector::readPGM( const char* fileName, int& width, int& height )
 {
    std::fstream file;
    file.open( fileName, std::ios::in );
-   
+   if( ! file )
+   {
+      std::cerr << "Unable to open the file " << fileName << std::endl;
+      return false;
+   }
+
    /***
     * Read header
     */
@@ -147,7 +152,7 @@ bool Vector::readPGM( const char* fileName, int& width, int& height )
       return false;
    }
    bool binary = ( magicNumber == "P5" );
-   
+
    /****
     * Skip comments and empty lines
     */
@@ -161,13 +166,13 @@ bool Vector::readPGM( const char* fileName, int& width, int& height )
 	    file.get( character );
    }
    file.unget();
- 
+
    /***
     * Read resolution
     */
    int maxColors;
    file >> width >> height >> maxColors;
- 
+
    /****
     * Allocate data
     */
@@ -196,18 +201,18 @@ void Vector::writePGM( const char* fileName, const int width, const int height )
 {
    std::fstream file;
    file.open( fileName, std::ios::out );
-   
+
    file << "P2\n";
    file << "# This file was generated at FJFI\n";
    file << width << ' '<< height << '\n' << "255\n";
-   
+
    for( int i = 0; i < height; i ++ )
    {
       for( int j = 0; j < width; j ++ )
       {
          int color = 255 * data[ i * width + j ];
          file << color << "\n";
-      }      
+      }
       file << '\n';
    }
 }
