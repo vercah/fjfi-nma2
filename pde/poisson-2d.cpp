fedaeed5e11110c6c367e7c7b139c1c224427b44
vercah <vercaposta@seznam.cz>
2023-06-16 15:08:04 +0200

    hw7 - finalize poisson

--- pde/poisson-2d.cpp
+++ pde/poisson-2d.cpp
@@ -7,6 +7,8 @@
 
 #include <fstream>
 #include <cmath>
+#include <iostream>
+using namespace std;
 #include "../Vector.h"
 #include "../matrices/EllpackMatrix.h"
 #include "../matrices/DenseMatrix.h"
@@ -18,8 +20,8 @@ const double h_sqr = h * h;
 
 double f( const double& x, const double& y )
 {
-   //return 10.0;
-   return 10.0 * x * y;
+   return 10.0;
+   //return 10.0 * x * y;
    //return 10.0 * ( 1.0 - 2.0 * x * x - 2.0 * y * y );
    //return 250.0 * x * x *sin( 10 * M_PI * ( x * x + y * y ) );
 }
@@ -33,20 +35,44 @@ int main( int argc, char* argv[] )
    Vector u( dofs ), b( dofs );
    EllpackMatrix A( dofs, dofs );
    A.setRowLength( 5 );
-     
-   for( int j = 0; j <= N; j++ )
-      for( int i = 0; i <= N; i++ )
+
+    for( int i = 0; i <= N; i++ ) {
+        for( int j = 0; j <= N; j++ ){
+            b[j*N+i]=f(j, i);
+        }
+    }
+
+   for( int i = 0; i < dofs; i++ ) {
+      for( int j = 0; j < dofs; j++ )
       {
          // Set-up the linear system matrix here.
+         // 1s on the diagonal
+         if(i<=(N+1) || i>=(N+1)*(N+1)-(N+2) || (i%(N+1)==0 && i>0) || (i+1)%(N+1)==0 && i>0){
+            if(j==i){
+                A.setElement(i, i, 1);
+                //cout << "1 ";
+            }else{//cout << "0 ";
+            }
+         }else if (j==(i-(N+1))||j==i+(N+1)||j==(i-1)||j==(i+1)){
+            A.setElement(i, j, -1);
+            //cout << "-1";
+         }else if (i==j){
+            A.setElement(i, j, 4);
+            //cout << "4 ";
+         }
+         else {//cout << "0 ";
+         }
       }
+      //cout << endl;
+   }
    
    /***
     * Solve the linear system
     */
    StationarySolver solver( A, b );
    solver.setConvergenceResidue( 1.0e-4 );
-   solver.setMaxIterations( 10000 );
-   solver.solve( u, "richardson", 1.0 );
+   solver.setMaxIterations( 500 );
+   solver.solve( u, "sor", 1.9 );
    
    /****
     * Write the solution to the output file
